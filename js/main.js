d0e33feef1a787ff2763234df3eb939a059c462d
Oliver Holt <osholt@me.com>
2020-02-17 21:25:28 +0000

    Dora
    Where Dora got up to

---
+++ js/main.js
@@ -0,0 +1,45 @@
+/****
+ERRA MAIN JS FILE
+Author: Aurora Widawati
+Date Created: 17/02/2020
+Date Modified: 17/02/2020
+*****/
+
+/** Scroll action **/
+$(document).ready(function () {
+  $(document).on("scroll", onScroll);
+
+  $('a[href^="#"]').on('click', function (e) {
+    e.preventDefault();
+    $(document).off("scroll");
+
+    $('a').each(function () {
+      $(this).removeClass('active');
+    })
+    $(this).addClass('active');
+
+    var target = this.hash;
+    $target = $(target);
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top+2
+    }, 500, 'swing', function () {
+      window.location.hash = target;
+      $(document).on("scroll", onScroll);
+    });
+  });
+});
+
+function onScroll(event){
+  var scrollPosition = $(document).scrollTop();
+  $('nav a').each(function () {
+    var currentLink = $(this);
+    var refElement = $(currentLink.attr("href"));
+    if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
+      $('nav ul li a').removeClass("active");
+      currentLink.addClass("active");
+    }
+    else{
+      currentLink.removeClass("active");
+    }
+  });
+}
