f4ebe5ec524e349c59d4d672e6b7a8f64b78c9cb
Aurora <aurorasw@live.com>
2020-03-07 10:54:05 +0000

    Restarted the navbar

--- js/main.js
+++ js/main.js
@@ -6,40 +6,11 @@ Date Modified: 17/02/2020
 *****/
 
 /** Scroll action **/
-$(document).ready(function () {
-  $(document).on("scroll", onScroll);
-
-  $('a[href^="#"]').on('click', function (e) {
-    e.preventDefault();
-    $(document).off("scroll");
-
-    $('a').each(function () {
-      $(this).removeClass('active');
-    })
-    $(this).addClass('active');
-
-    var target = this.hash;
-    $target = $(target);
-    $('html, body').stop().animate({
-      'scrollTop': $target.offset().top+2
-    }, 500, 'swing', function () {
-      window.location.hash = target;
-      $(document).on("scroll", onScroll);
-    });
-  });
-});
-
-function onScroll(event){
-  var scrollPosition = $(document).scrollTop();
-  $('nav a').each(function () {
-    var currentLink = $(this);
-    var refElement = $(currentLink.attr("href"));
-    if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
-      $('nav ul li a').removeClass("active");
-      currentLink.addClass("active");
-    }
-    else{
-      currentLink.removeClass("active");
-    }
-  });
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
